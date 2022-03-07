a9584023f9fab49ed938db5295f1f96e8af27059
Shreyas Rana <ranashreyas@gmail.com>
2022-03-06 18:40:40 -0800

    Converted the test intake command to a sequential command group

---
+++ src/main/java/frc/robot/commands/IntakeFeeder/TestIntakeCommandGroup.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.IntakeFeeder;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.IntakeSubsystem.IntakeState;
+
+public class TestIntakeCommandGroup extends SequentialCommandGroup {
+    public TestIntakeCommandGroup(IntakeSubsystem intake) {
+        if (intake.getIntakeState() == IntakeState.UP) {
+            addCommands(
+                new DeployIntakeCommand(intake, IntakeState.DOWN),
+                new DeployIntakeCommand(intake, IntakeState.UP)
+            );
+        } else {
+            addCommands(
+                new DeployIntakeCommand(intake, IntakeState.UP),
+                new DeployIntakeCommand(intake, IntakeState.DOWN),
+                new DeployIntakeCommand(intake, IntakeState.UP)
+            );
+        }
+    }
+}

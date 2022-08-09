269623d5b38b46b606ccb83055c49bd960b965ee
sjwang05 <63834813+sjwang05@users.noreply.github.com>
2022-08-09 12:40:36 -0700

    Improve robot code init test

--- src/test/java/test/RobotCodeTest.java
+++ src/test/java/test/RobotCodeTest.java
@@ -22,15 +22,45 @@ public class RobotCodeTest {
         RobotContainer m_container = new RobotContainer();
         assertNotNull("Failed to initialize RobotContainer.", m_container);
 
+        // There's probably a way better way to do this
+        // TODO: that ^
         ArrayList<SubsystemBase> m_subsystems = new ArrayList<>();
-        m_subsystems.add(DrivetrainSubsystem.getInstance());
-        m_subsystems.add(FeederSubsystem.getInstance());
-        m_subsystems.add(HangSubsystem.getInstance());
-        m_subsystems.add(IntakeSubsystem.getInstance());
-        m_subsystems.add(LEDSubsystem.getInstance());
-        m_subsystems.add(ShooterSubsystem.getInstance());
-        m_subsystems.add(VisionSubsystem.getInstance());
-
+        try {
+            m_subsystems.add(DrivetrainSubsystem.getInstance());
+        } catch (Exception e) {
+            fail("Failed to initialize " + m_subsystems.get(0).getName());
+        }
+        try {
+            m_subsystems.add(FeederSubsystem.getInstance());
+        } catch (Exception e) {
+            fail("Failed to initialize " + m_subsystems.get(1).getName());
+        }
+        try {
+            m_subsystems.add(HangSubsystem.getInstance());
+        } catch (Exception e) {
+            fail("Failed to initialize " + m_subsystems.get(2).getName());
+        }
+        try {
+            m_subsystems.add(IntakeSubsystem.getInstance());
+        } catch (Exception e) {
+            fail("Failed to initialize " + m_subsystems.get(3).getName());
+        }
+        try {
+            m_subsystems.add(LEDSubsystem.getInstance());
+        } catch (Exception e) {
+            fail("Failed to initialize " + m_subsystems.get(4).getName());
+        }
+        try {
+            m_subsystems.add(ShooterSubsystem.getInstance());
+        } catch (Exception e) {
+            fail("Failed to initialize " + m_subsystems.get(5).getName());
+        }
+        try {
+            m_subsystems.add(VisionSubsystem.getInstance());
+        } catch (Exception e) {
+            fail("Failed to initialize " + m_subsystems.get(6).getName());
+        }
+        
         for (int i = 0; i < m_subsystems.size(); ++i) {
             assertNotNull("Failed to initialize " + m_subsystems.get(i).getName(), m_subsystems.get(i));
         }
